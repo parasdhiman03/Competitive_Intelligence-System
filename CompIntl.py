68977e21cb2e3f8c8282b8b76f843cdfdb7ef0dd
Paras Dhiman <115029469+parasdhiman03@users.noreply.github.com>
2023-08-16 21:49:39 +0530

    Update CompIntl.py

--- CompIntl.py
+++ CompIntl.py
@@ -9,7 +9,7 @@ from streamlit_lottie import st_lottie
 import nltk
 import streamlit as st;
 
-st.set_page_config(page_title="Stocks",page_icon=":tada:",layout="wide")
+st.set_page_config(page_title="Product Insights",page_icon=":tada:",layout="wide")
 
 def perform_topic_modeling(data_path, num_topics=5, passes=5):
     def preprocess_text(texts, allowed_postags=["NOUN", "VERB", "ADJ", "ADV"], stopwords=None):
